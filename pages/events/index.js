8591a84cb58390eb14576b32ffaf309870368b23
Attila Horvath <sweeptic@gmail.com>
2025-03-11 10:10:41 +0100

    012 Working on the All Events Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import EventList from '../../components/event-list';
+import { getAllEvents } from '../../dummy-data';
+
 export default function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All Events page</h1>
+      <EventList featuredEvents={events} />
     </div>
   );
 }
