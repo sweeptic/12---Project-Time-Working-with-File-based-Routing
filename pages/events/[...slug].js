6405a1955b27d3769458282f682dcefd8084b76d
Attila Horvath <sweeptic@gmail.com>
2025-03-11 11:32:02 +0100

    016 Final Steps

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/router';
 import { getFilteredEvents } from '../../dummy-data';
 import EventList from '../../components/event-list';
+import ResultsTitle from '../../components/results-title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert';
 
 export default function FilteredEventsPage() {
   const router = useRouter();
@@ -16,14 +19,39 @@ export default function FilteredEventsPage() {
   const monthNum = +filterData[1];
 
   if (isNaN(yearNum) || isNaN(monthNum) || yearNum > 2030 || yearNum < 2021 || monthNum < 1 || monthNum > 12) {
-    return <p>Invalid filter.</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid filter.</p>
+          <div className="center">
+            <Button link="/events">Show All Events</Button>
+          </div>
+        </ErrorAlert>
+      </>
+    );
   }
 
   const filteredEvents = getFilteredEvents({ year: yearNum, month: monthNum });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for the chosen filter!</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+          <div className="center">
+            <Button link="/events">Show All Events</Button>
+          </div>
+        </ErrorAlert>
+      </>
+    );
   }
 
-  return <div>{<EventList featuredEvents={filteredEvents} />}</div>;
+  const date = new Date(yearNum, monthNum - 1);
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList featuredEvents={filteredEvents} />
+    </>
+  );
 }
