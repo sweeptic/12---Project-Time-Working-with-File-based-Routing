3d021672e5c90b95787bd016661ab62a26e9b7d5
Attila Horvath <sweeptic@gmail.com>
2025-03-11 08:05:46 +0100

    006 Adding More React Components & Connecting Components

--- components/event-item.js
+++ components/event-item.js
@@ -1,10 +1,47 @@
-export default function EventItem(props) {
-  const { id, title, description } = props.event;
+import Link from 'next/link';
+
+export default function EventItem({ event }) {
+  const { id, image, title, description, date, location } = event;
+
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const formattedAddress = location.replace(', ', '\n');
+
+  const exploreLink = `/events/{$id}`;
 
   return (
     <li key={id}>
-      <h1> {title}</h1>
-      <p>{description}</p>
+      <img src={image} alt={title} />
+      <div>
+        <div>
+          <h2> {title}</h2>
+          <div>
+            <time dateTime="">{formattedDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
     </li>
   );
 }
+
+//   {
+//     id: 'e1',
+//     title: 'Programming for everyone',
+//     description:
+//       'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
+//     location: 'Some street 25, 12345 San Some where',
+//     date: '2021-05-12',
+//     image: 'images/coding-event.jpg',
+//     isFeatured: false,
+//   },
--- components/event-list.js
+++ components/event-list.js
@@ -1,16 +1,7 @@
 import EventItem from './event-item';
 
 export default function EventList(props) {
-  //  id: 'e1',
-  //  title: 'Programming for everyone',
-  //  description:
-  //       'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
-  //  location: 'Some street 25, 12345 San Some where',
-  //  date: '2021-05-12',
-  //  image: 'images/coding-event.jpg',
-  //  isFeatured: false,
+  const eventList = props.featuredEvents.map((event) => <EventItem event={event} />);
 
-  const eventsList = props.featuredEvents.map((event) => <EventItem event={event} />);
-
-  return <ul>{eventsList}</ul>;
+  return <ul>{eventList}</ul>;
 }
