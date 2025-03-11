6f04b25603886ddee2dd330bc3bbb37a38a220a5
Attila Horvath <sweeptic@gmail.com>
2025-03-11 08:48:19 +0100

    009 Adding Buttons & Icons

--- components/event-item.js
+++ components/event-item.js
@@ -1,5 +1,9 @@
 import Link from 'next/link';
 import classes from './event-item.module.css';
+import Button from './ui/button';
+import DateIcon from './icons/date-icon';
+import AddressIcon from './icons/address-icon';
+import ArrowRightIcon from './icons/arrow-right-icon';
 
 export default function EventItem({ event }) {
   const { id, image, title, description, date, location } = event;
@@ -21,15 +25,22 @@ export default function EventItem({ event }) {
         <div className={classes.summary}>
           <h2> {title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time dateTime="">{formattedDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
 
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
